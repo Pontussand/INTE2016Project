0f285230d89e0c4d87e41c345b7682eb44f5af4d
Annika Svedin <annika.svedin@gmail.com>
2016-10-11 10:48:45 +0200

    List the content of a local folder, method and test

--- CommandLine/src/main/java/CommandLine.java
+++ CommandLine/src/main/java/CommandLine.java
@@ -1,3 +1,30 @@
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
 public class CommandLine {
+
+
+    public String listFolders() {
+
+//        local files on annikas mac
+        String dirPath = "/Users/per/Dev/annika/INTE/FinalProject/testFolder";
+        File dir = new File(dirPath);
+        String[] files = dir.list();
+
+        String str = "";
+
+        if (files.length == 0) {
+            System.out.println("The directory is empty");
+
+        } else {
+
+            for (String aFile : files) {
+                System.out.println(aFile);
+                str += aFile;
+            }
+        }
+
+        return str;
+    }
 }
--- CommandLine/src/test/java/CommandLineTest.java
+++ CommandLine/src/test/java/CommandLineTest.java
@@ -1,3 +1,17 @@
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
 
 public class CommandLineTest {
+
+    @Test
+    public void listTest() {
+
+        CommandLine cm = new CommandLine();
+
+        String expectedString = "untitled folder";
+
+        assertEquals(expectedString, cm.listFolders());
+
+    }
+
 }
