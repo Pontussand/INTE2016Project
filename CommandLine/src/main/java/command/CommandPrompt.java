0744733b276681d8e5dee24db200aa0e63ab86d8
Pontussand <pontussandliden@hotmail.com>
2016-10-12 13:25:05 +0200

    more whitespaces

--- CommandLine/src/main/java/command/CommandPrompt.java
+++ CommandLine/src/main/java/command/CommandPrompt.java
@@ -11,6 +11,9 @@ public class CommandPrompt {
 			command(command);
 		}
 	}
+	
+	
+	
 	public void command(String command) {
 		String commandPart = command.split(" ")[0];
 		String target = command.split(" ")[command.indexOf(" ")];
