85d971f10fcb4a207352151af9bea556c320a8a9
Felix Törnqvist <felix.trnqvist@gmail.com>
2016-10-12 16:10:46 +0200

    TDD in progress..

---
+++ CommandLine/src/main/java/fileSystemObject/FakeFSO.java
@@ -0,0 +1,16 @@
+package fileSystemObject;
+
+/**
+ * Created by felix on 2016-10-12.
+ */
+public class FakeFSO {
+
+	public FakeFSO(String path){
+
+	}
+
+
+	public FakeFSO[] listFiles(){
+		return null;
+	}
+}
---
+++ CommandLine/src/test/java/fileSystemObject/fakeFileSystemObjectTest.java
@@ -0,0 +1,28 @@
+package fileSystemObject;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class fakeFileSystemObjectTest {
+	FakeFSO root;
+	String testDir1 = "testDir1";
+
+	@Before
+	public void setup(){
+		root = new FakeFSO("/");
+	}
+
+	@Test
+	public void addDirectory(){
+		FakeFSO newDir = new FakeFSO("/"+testDir1);
+
+		FakeFSO[] expectedListing = {new FakeFSO("testDir1")};
+		FakeFSO[] listing = root.listFiles();
+	}
+}
