14bf1a516c69c0b8d3acd464a44e719033796272
Annika Svedin <annika.svedin@gmail.com>
2016-10-13 13:44:52 +0200

    re-created Filemanager

---
+++ CommandLine/src/main/java/command/Filemanager.java
@@ -0,0 +1,28 @@
+package command;
+
+import file_system_adapter.FileSystemAdapter;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Filemanager {
+    
+
+    void ls(File path) {
+
+    }
+
+    void findProjectDir() {
+
+    }
+
+    void findParentDir(File currentDir) {
+
+    }
+
+    void mkdir(File newDir) {
+
+    }
+
+
+}
