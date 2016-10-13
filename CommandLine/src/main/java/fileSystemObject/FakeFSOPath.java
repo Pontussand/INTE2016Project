52815c2a0ce2b5cae57ecd3c67cc4f8afbfec2fb
Annika Svedin <annika.svedin@gmail.com>
2016-10-13 11:59:15 +0200

    code forematting

--- CommandLine/src/main/java/fileSystemObject/FakeFSOPath.java
+++ CommandLine/src/main/java/fileSystemObject/FakeFSOPath.java
@@ -1,22 +1,20 @@
 package fileSystemObject;
 
-/**
- * Created by felix on 2016-10-12.
- */
+
 public class FakeFSOPath {
 	String path;
 
-	public FakeFSOPath(String path){
+	public FakeFSOPath(String path) {
 		this.path = path;
 	}
 
 
-	public FakeFSOPath[] listFiles(){
+	public FakeFSOPath[] listFiles() {
 		return null;
 	}
 
-	public boolean equals(Object other){
-		if(other instanceof FakeFSOPath){
+	public boolean equals(Object other) {
+		if(other instanceof FakeFSOPath) {
 			FakeFSOPath o = (FakeFSOPath) other;
 			o.path.equals(path);
 		}
