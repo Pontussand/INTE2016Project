c163127af31b4f6c55ff0376e9ca3018953f55dc
Felix Törnqvist <felix.trnqvist@gmail.com>
2016-10-13 11:57:08 +0200

    refactoring, developing

--- CommandLine/src/main/java/fileSystemObject/FakeDir.java
+++ CommandLine/src/main/java/fileSystemObject/FakeDir.java
@@ -1,25 +1,32 @@
 package fileSystemObject;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 
 public class FakeDir extends FakeFSO{
-	private HashSet<FakeFSO> contents = new HashSet<>();
+	private HashMap<String, FakeFSO> contents = new HashMap<>();
 
 	public FakeDir(String name){
 		super(name);
 	}
 
 	public boolean addFSO(FakeFSO content){
-		return contents.add(content);
+		boolean exists = contents.containsKey(content.getName());
+		if(!exists)
+			contents.put(content.getName(),content);
+		return exists;
 	}
 
 	public String[] listContents(){
 		String[] ret = new String[contents.size()];
 		int retInd = 0;
-		for(FakeFSO c : contents){
+		for(FakeFSO c : contents.values()){
 			ret[retInd++] = c.getName();
 		}
 		return ret;
+	}
+
+	public FakeFSO searchByPath(String path){
+		String localFSO = path.split(FakePath.DIR_SEPARATOR, 2)[0];
+		return contents.get(localFSO);
 	}
 }
