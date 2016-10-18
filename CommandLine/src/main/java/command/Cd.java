1cff677dd45863084915de4d7c5898abd91c3235
Annika Svedin <annika.svedin@gmail.com>
2016-10-18 11:44:45 +0200

    created class Cd

---
+++ CommandLine/src/main/java/command/Cd.java
@@ -0,0 +1,12 @@
+package command;
+
+import file_system_adapter.FileSystemAdapter;
+
+public class Cd extends Command {
+
+	public String doCommand(String currentDir, String input) {
+
+		return "";
+
+	}
+}
