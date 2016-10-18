9f6a225578252b20aaa75f9943aa70cfa0e495f2
Felix Törnqvist <felix.trnqvist@gmail.com>
2016-10-18 14:00:20 +0200

    continue working on Cd command class with TDD

--- CommandLine/src/test/java/prompt/command/CdTest.java
+++ CommandLine/src/test/java/prompt/command/CdTest.java
@@ -2,26 +2,40 @@ package prompt.command;
 
 import file_system_adapter.FakeFileSystemAdapter;
 import file_system_adapter.fake_FSO.FakeDirectory;
-import file_system_adapter.fake_FSO.FakeFSO;
 import org.junit.Before;
 import org.junit.Test;
+import prompt.util.Path;
 
 import static org.junit.Assert.assertEquals;
 
-
 public class CdTest {
 
-	private Ls ls;
+	private Cd cd;
 	private FakeFileSystemAdapter fakeAdapter;
 	private FakeDirectory root;
 
+	private Path currentPath;
+
 	@Before
 	public void before() {
-		ls = new Ls();
-		fakeAdapter = new FakeFileSystemAdapter();
+		cd = new Cd();
+
 		root = new FakeDirectory("root");
+		fakeAdapter = new FakeFileSystemAdapter();
 		fakeAdapter.setRoot(root);
-		ls.setAdapter(fakeAdapter);
+		Command.setAdapter(fakeAdapter);
+
+		//root
+		currentPath = new Path(Path.DIR_SEPERATOR);
+	}
+
+	@Test
+	public void doCommand_goToSubfolder(){
+		FakeDirectory sub = new FakeDirectory("subfolder");
+		root.addFSO(sub);
+
+		cd.doCommand(currentPath, "subfolder");
+		assertEquals("/subfolder", currentPath.getPath());
 	}
 
 
