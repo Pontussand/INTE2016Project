7cec499843b7fc4e4925b7544133b26fddda55ce
naicah <ni.hedman@gmail.com>
2016-10-20 16:21:57 +0200

    Deleted tests for old switch in command in CommandPrompt

--- CommandLine/src/test/java/prompt/CommandPromptTest.java
+++ CommandLine/src/test/java/prompt/CommandPromptTest.java
@@ -8,11 +8,6 @@ import static org.junit.Assert.assertEquals;
 
 public class CommandPromptTest {
 
-	private static String ls_filename = "ls abc";
-	private static String ls_expected = "ls fil: abc";
-	private static String mkdir_filename = "mkdir abc";
-	private static String mkdir_expected = "mkdir fil: abc";
-	private static String wrongCommand = "Command doesn't exit";
 	private static String expected20commands = "lsfdptouchmkdirlsfdptouchmkdirlsfdptouchmkdirlsfdptouchmkdirlsfdptouchmkdir";
 
 	CommandPrompt cp;
@@ -34,38 +29,5 @@ public class CommandPromptTest {
 		}
 	}
 
-	@Test
-	public void command_lsFileName(){
-		assertEquals(ls_expected, cp.command(ls_filename));
-	}
 
-	@Test
-	public void command_twoExclemetionMarks(){
-		assertEquals(expected20commands, cp.command("!!"));
-	}
-
-	@Test
-	public void command_mkdirFileName(){
-		assertEquals(mkdir_expected, cp.command(mkdir_filename));
-	}
-
-	@Test
-	public void command_wrongCommand(){
-		assertEquals(wrongCommand, cp.command("3 abc"));
-	}
-
-	@Test
-	public void command_lsNoFileName(){
-		assertEquals("ls fil: ", cp.command("ls "));
-	}
-
-	@Test
-	public void command_pwd(){
-		assertEquals("pwd", cp.command("pwd"));
-	}
-
-	@Test
-	public void command_lsFileNameNoSpace(){
-		assertEquals(wrongCommand, cp.command("lsabc"));
-	}
 }
