ee07a391202a8541b4132c6624270374f746602c
naicah <ni.hedman@gmail.com>
2016-10-21 20:48:19 +0200

    Added command RepeatFromHistory + it's tests

---
+++ CommandLine/src/main/java/prompt/command/RepeatFromHistory.java
@@ -0,0 +1,41 @@
+package prompt.command;
+
+import prompt.CommandPrompt;
+import prompt.util.PathContainer;
+
+public class RepeatFromHistory extends Command {
+
+    public final static String INVALID_NUMBER = " is not a number, try again!";
+    public final static String OUT_OF_BOUNDS_HISTORY = "You haven't made that many commands, try again!";
+
+    public RepeatFromHistory(CommandPrompt prompt) {
+        super(prompt);
+    }
+
+    public String getName(){
+        return "!";
+    }
+
+    public String doCommand(PathContainer currentDir, String input) {
+        int index;
+        try{
+            index = Integer.parseInt(input);
+        }catch(Exception e){
+            return input + INVALID_NUMBER;
+        }
+
+        if(commandHistory.isEmpty()){
+            return RepeatLast.NO_PREVIOUS_COMMAND;
+        }else if(index >= commandHistory.size()){
+            return OUT_OF_BOUNDS_HISTORY;
+        }
+        String commandToRepeat = commandHistory.get(index);
+        String result = ownerCommandPrompt.command(commandToRepeat);
+        return result;
+    }
+
+    @Override
+    protected boolean shouldBeAddedToHistory(){
+        return false;
+    }
+}
---
+++ CommandLine/src/test/java/prompt/command/RepeatFromHistoryTest.java
@@ -0,0 +1,56 @@
+package prompt.command;
+
+import file_system_adapter.FakeFileSystemAdapter;
+import file_system_adapter.fake_FSO.FakeDirectory;
+import org.junit.Before;
+import org.junit.Test;
+import prompt.CommandPrompt;
+import prompt.util.PathContainer;
+
+import static org.junit.Assert.assertEquals;
+import static prompt.command.Command.commandHistory;
+
+public class RepeatFromHistoryTest {
+
+    private CommandPrompt prompt;
+    private RepeatFromHistory repeatFromHistory;
+    private FakeFileSystemAdapter fakeAdapter;
+    private FakeDirectory root;
+    private PathContainer currentDir;
+    private String input = null;
+
+    @Before
+    public void before() {
+        root = new FakeDirectory("root");
+        fakeAdapter = new FakeFileSystemAdapter();
+        fakeAdapter.setRoot(root);
+        Command.setAdapter(fakeAdapter);
+        currentDir = new PathContainer("");
+        repeatFromHistory = new RepeatFromHistory(prompt = new CommandPrompt(fakeAdapter));
+        commandHistory.clear();
+    }
+
+    @Test
+    public void doCommand_empty() {
+        assertEquals(RepeatLast.NO_PREVIOUS_COMMAND, repeatFromHistory.doCommand(currentDir, "3"));
+    }
+
+    @Test
+    public void doCommand_repeatHistory() {
+        commandHistory.add("history");
+        assertEquals("0 history\n1 history", repeatFromHistory.doCommand(currentDir, "0"));
+    }
+
+    @Test
+    public void doCommand_notANumber() {
+        commandHistory.add("ls");
+        assertEquals("not a number is not a number, try again!", repeatFromHistory.doCommand(currentDir, "not a number"));
+    }
+
+    @Test
+    public void doCommand_outOfBounds() {
+        commandHistory.add("ls");
+        assertEquals("You haven't made that many commands, try again!", repeatFromHistory.doCommand(currentDir, "6"));
+    }
+
+}
