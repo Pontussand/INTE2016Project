8189de48ad026b1448e5b14baf6df3441b75fdbe
Pontussand <pontussandliden@hotmail.com>
2016-10-26 14:18:50 +0200

    added lstest tests

--- CommandLine/src/test/java/prompt/command/LsTest.java
+++ CommandLine/src/test/java/prompt/command/LsTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 import prompt.CommandPrompt;
 import prompt.util.PathContainer;
 
+import java.nio.file.Path;
+
 import static org.junit.Assert.assertEquals;
 
 
@@ -31,6 +33,7 @@ public class LsTest {
 		input = "";
 
 		commandPrompt = new CommandPrompt(fakeAdapter);
+		Pwd pwd = new Pwd(commandPrompt);
 		ls = new Ls(commandPrompt);
 		ls.setAdapter(fakeAdapter);
 		mkdir = new Mkdir(commandPrompt);
@@ -41,6 +44,7 @@ public class LsTest {
 		fakeDir1 = new FakeDirectory("FirstFolder");
 
 		fakeDir2 = new FakeDirectory("SecondFolder");
+		currentDir = new PathContainer(PathContainer.getFullPath("", "FirstFolder"));
 		fakeDir2.addFSO(new FakeDirectory("TestFolder1"));
 		fakeDir2.addFSO(new FakeDirectory("TestFolder2"));
 		fakeDir2.addFSO(new FakeDirectory("TestFolder3"));
@@ -77,17 +81,23 @@ public class LsTest {
 
 	@Test
 	public void doCommand_LsAbsolutePath_Bt2(){
-
+		String expected = "TestFile1\nTestFile2\nTestFolder1\nTestFolder2\nTestFolder3\nTestFolder4\n";
+		assertEquals(expected, ls.doCommand(currentDir, "/FirstFolder/SecondFolder"));
+		assertEquals(expected, commandPrompt.command("ls /FirstFolder/SecondFolder"));
 	}
 
 	@Test
 	public void doCommand_lsParent_Bt3(){
-
+		String expected = "SecondFolder";
+		assertEquals(expected, ls.doCommand(currentDir, ".."));
+	//	assertEquals(expected, commandPrompt.command("ls .."));
 	}
 
 	@Test
 	public void doCommand_Ls_Bt4(){
-
+		String expected = "TestFile1\nTestFile2\nTestFolder1\nTestFolder2\nTestFolder3\nTestFolder4\n";
+		assertEquals(expected, ls.doCommand(currentDir, ""));
+		assertEquals(expected, commandPrompt.command("ls"));
 	}
 
 	@Test
@@ -99,17 +109,23 @@ public class LsTest {
 
 	@Test
 	public void doCommand_LsDirsAbsolutePath_Bt6(){
-
+		String expected = "TestFolder1\nTestFolder2\nTestFolder3\nTestFolder4\n";
+		assertEquals(expected, ls.doCommand(currentDir, "/FirstFolder/SecondFolder -dirs"));
+		assertEquals(expected, commandPrompt.command("ls /FirstFolder/SecondFolder -dirs"));
 	}
 
 	@Test
 	public void doCommand_LsDirsParent_Bt7(){
-
+		String expected = "SecondFolder";
+		assertEquals(expected, ls.doCommand(currentDir, ".. -dirs"));
+		assertEquals(expected, commandPrompt.command("ls .. -dirs"));
 	}
 
 	@Test
-	public void doCommand_Ls_Bt8(){
-
+	public void doCommand_LsDirs_Bt8(){
+		String expected = "SecondFolder";
+		assertEquals(expected, ls.doCommand(currentDir, "-dirs"));
+		assertEquals(expected, commandPrompt.command("ls -dirs"));
 	}
 
 	@Test
@@ -117,6 +133,47 @@ public class LsTest {
 		String expected = "TestFile1\nTestFile2\n";
 		assertEquals(expected, ls.doCommand(currentDir, "FirstFolder/SecondFolder -files"));
 		assertEquals(expected, commandPrompt.command("ls FirstFolder/SecondFolder -files"));
+	}
+
+	@Test
+	public void doCommand_LsFilesAbsolutePath_Bt10(){
+		String expected = "TestFolder1\nTestFolder2\nTestFolder3\nTestFolder4\n";
+		assertEquals(expected, ls.doCommand(currentDir, "/FirstFolder/SecondFolder -files"));
+		assertEquals(expected, commandPrompt.command("ls /FirstFolder/SecondFolder -files"));
+	}
+
+	@Test
+	public void doCommand_LsFilesAbsolutePath_Bt11(){
+		String expected = "TestFolder1\nTestFolder2\nTestFolder3\nTestFolder4\n";
+		assertEquals(expected, ls.doCommand(currentDir, ".. -files"));
+		assertEquals(expected, commandPrompt.command("ls .. -files"));
+	}
+
+	@Test
+	public void doCommand_Ls_Bt12(){
+		String expected = "TestFolder1\nTestFolder2\nTestFolder3\nTestFolder4\n";
+		assertEquals(expected, ls.doCommand(currentDir, "-files"));
+		assertEquals(expected, commandPrompt.command("ls -files"));
+	}
+
+	@Test
+	public void doCommand_Ls_Bt13(){
+
+	}
+
+	@Test
+	public void doCommand_Ls_Bt14(){
+
+	}
+
+	@Test
+	public void doCommand_Ls_Bt15(){
+
+	}
+
+	@Test
+	public void doCommand_Ls_Bt16(){
+
 	}
 
 
